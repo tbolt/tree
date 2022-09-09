94382ed8bdcb5a938ed99f3fd99d4d7c475be81e
Ty Bolt <tybolt@gmail.com>
2022-09-09 09:38:11 -0400

    init

---
+++ db/migrate/20220821171250_add_username_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddUsernameColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string, null: false, unique: true
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_08_21_192818) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_sessions", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "remember_token", null: false
+    t.string "user_agent"
+    t.string "ip_address"
+    t.index ["remember_token"], name: "index_active_sessions_on_remember_token", unique: true
+    t.index ["user_id"], name: "index_active_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest", null: false
+    t.string "username", null: false
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "active_sessions", "users", on_delete: :cascade
+end
